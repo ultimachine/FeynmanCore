0d4ee541a827f39da118e51931de9db7d9c64de4
tonokip <tonokip@gmail.com>
2017-09-05 17:15:34 -0500

    update chip.h to include peripheral drivers

--- cores/core3/asf/chip.h
+++ cores/core3/asf/chip.h
@@ -1,1 +1,26 @@
-#include <sam.h>
+#include <compiler.h>
+#include <status_codes.h>
+
+#include "conf_clock.h"
+
+#include <sam.h> //Include SAMG55 CMSIS
+
+#include "interrupt_sam_nvic.h"
+
+//Include SAM peripheral drivers.
+#include "adc2.h"
+#include "efc.h"
+#include "flexcom.h"
+#include "matrix.h"
+#include "pio.h"
+#include "pmc.h"
+#include "sleep.h"
+#include "supc.h"
+#include "tc.h"
+#include "udp_device.h"
+#include "usart.h"
+#include "wdt.h"
+
+#include "osc.h"
+#include "pll.h"
+#include "sysclk.h"
