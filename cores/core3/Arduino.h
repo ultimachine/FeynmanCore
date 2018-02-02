14c8666b2530417f99d1810cb529fdb54710bf89
tonokip <tonokip@gmail.com>
2018-02-02 15:18:23 -0600

    fix for arduino due compatibility. add back missing math and AVR headers.

--- cores/core3/Arduino.h
+++ cores/core3/Arduino.h
@@ -23,6 +23,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+// some libraries and sketches depend on this
+// AVR stuff, assuming Arduino.h or WProgram.h
+// automatically includes it...
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
 #include "binary.h"
 #include "itoa.h"
 
